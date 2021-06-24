9d0283062b25e5f443c7c66837bd2ddc435dbd66
sujin <sujinee9797@gmail.com>
2021-06-24 18:57:38 +0900

    first commit
    프로젝트 생성

---
+++ src/main/java/com/example/gwanggo/GwanggoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gwanggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwanggoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwanggoApplication.class, args);
+    }
+
+}
