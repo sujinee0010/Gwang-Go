6f016c8fc89c7d47becf96aab3265c3b96ba7999
sujin <sujinee9797@gmail.com>
2021-06-25 11:57:42 +0900

    add authorization & user api
    jwt 인증 / user 로그인,회원가입,태그추가 api 생성

---
+++ src/main/java/com/example/gwanggo/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.example.gwanggo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 45)
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 1, max = 1)
+    private String sex;
+}
---
+++ src/main/java/com/example/gwanggo/repository/UserTagRepository.java
@@ -0,0 +1,16 @@
+package com.example.gwanggo.repository;
+
+
+import com.example.gwanggo.entity.Tag;
+import com.example.gwanggo.entity.UserTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserTagRepository extends JpaRepository<UserTag, Long> {
+
+}
